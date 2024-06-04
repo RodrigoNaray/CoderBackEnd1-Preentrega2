d2afed60eb33c75ab671b20988a351485b672d5e
Rodrigo Naray <rodrigo.naray@fing.edu.uy>
2024-06-04 20:59:11 -0300

    PreEntrega2

--- src/app.js
+++ src/app.js
@@ -1,17 +1,35 @@
 import express from "express";
-import routes from "./router/index.js";
+import routes from "./routes/index.js";
+import viewsRoutes from "./routes/views.routes.js";
+import __dirname from "./dirname.js";
 import handlebars from "express-handlebars";
+import { Server } from "socket.io";
+import productManager from "./productManager.js";
 
-const PORT = 8080
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.engine("handlebars", handlebars.engine()); 
+app.set("views", __dirname + "/views"); 
+app.set("view engine", "handlebars"); 
+app.use(express.static("public"));
 
 
-app.use("/api", routes)
+app.use("/api", routes);
 
+app.use("/", viewsRoutes)
 
-app.listen(PORT, () => {
-  console.log(`Servidor funcionando en puerto ${PORT}`)
-})
+const PORT = 8080;
+
+const httpServer = app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT} , Link: http://localhost:${PORT}/`);
+});
+
+export const io = new Server(httpServer);
+
+io.on("connection", async (socket) => {
+  console.log("Nuevo Cliente Conectado");
+  const products = await productManager.getProducts();
+  socket.emit("products", products);
+});
